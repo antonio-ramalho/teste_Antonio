81ec63bab265a595442ce71828fdc6b55afcb3af
antonio <antoninho.ferreira2015@gmail.com>
2026-02-01 16:03:02 -0300

    Sprint: leitura e gravação de arquivos

--- src/main/java/org/projeto/application/Program.java
+++ src/main/java/org/projeto/application/Program.java
@@ -1,4 +1,84 @@
 package org.projeto.application;
 
+import org.projeto.entities.Operadora;
+import org.projeto.exceptions.DomainException;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Program {
+    public static void main(String[] args) {
+        SimpleDateFormat sdfAno =  new SimpleDateFormat("yyyy");
+
+        Map <String, Operadora> operadoras = lerOperadora();
+        lerDespesas(operadoras);
+        registroAgregado(operadoras);
+    }
+
+    public static Map <String, Operadora> lerOperadora() {
+        Map <String, Operadora> Operadoras = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\temp\\Relatorio_cadop.csv"))) {
+            String[] campos = new String[19];
+            String linha = br.readLine();
+
+            linha = br.readLine();
+            while (linha != null) {
+                campos = linha.split(";");
+
+                for (int i=0; i < campos.length; i++) {
+                    campos[i] = campos[i].replace("\"", "");
+                }
+
+                Operadora op = new Operadora(campos[0],  campos[1],
+                        campos[2], campos[4], campos[10]);
+
+                Operadoras.put(campos[0], op);
+                linha = br.readLine();
+            }
+        }
+        catch (IOException | DomainException e) {
+            throw new DomainException("Não foi possível ler o arquivo das operadoras!");
+        }
+        return Operadoras;
+    }
+
+    public static void lerDespesas(Map <String, Operadora> operadoras) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\temp\\1T2025.csv"))) {
+            String[] campos = new String[5];
+            String line = br.readLine();
+
+            while (line != null) {
+                campos = line.split(";");
+                if (campos[3].equalsIgnoreCase("\"PUBLICIDADE E PROPAGANDA\"")) {
+                    for (int i=0; i < campos.length; i++) {
+                        campos[i] = campos[i].replace("\"", "");
+                    }
+                    Operadora operadoraInstanciada = operadoras.get(campos[1]);
+                    if (operadoraInstanciada != null) {
+                        operadoraInstanciada.setAno(campos[0]);
+                        operadoraInstanciada.somaDespesas(Double.parseDouble(campos[5].replace(",", ".")));
+                    }
+                }
+                line = br.readLine();
+            }
+        } catch (IOException | DomainException e) {
+            throw new DomainException("Não foi possível ler o arquivo das despesas!");
+        }
+    }
+
+    public static void registroAgregado(Map <String, Operadora> operadoras) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\temp\\produtosOut.csv"))) {
+            for (Operadora operadora : operadoras.values()) {
+                bw.write(operadora.toString());
+                bw.newLine();
+            }
+        } catch (IOException | DomainException e) {
+            throw new DomainException("Não foi possível gravar as despesas agregadas!");
+        }
+    }
 }
--- src/main/java/org/projeto/entities/Operadora.java
+++ src/main/java/org/projeto/entities/Operadora.java
@@ -1,28 +1,46 @@
 package org.projeto.entities;
 
-public class Empresa {
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Operadora {
     private String cnpj;
     private String razaoSocial;
     private String registroAns;
-    private String trimestre;
-    private String ano;
-    private String valorDespesas;
+    private LocalDate ano;
+    private BigDecimal valorDespesas;
     private String uf;
     private String modalidade;
 
-    public Empresa() {
+    public Operadora() {
     }
 
-    public Empresa(String registroAns, String cnpj, String razaoSocial, String modalidade, String uf) {
+    public Operadora(String registroAns, String cnpj, String razaoSocial, String modalidade, String uf) {
         this.registroAns = registroAns;
         this.cnpj = cnpj;
         this.razaoSocial = razaoSocial;
         this.modalidade = modalidade;
         this.uf = uf;
+        this.valorDespesas = BigDecimal.ZERO;
+    }
+
+    public void setAno(String ano) {
+        LocalDate localDate = LocalDate.parse(ano);
+        int localAno = localDate.getYear();
+        this.ano = LocalDate.of(localAno, 1, 1);
+    }
+
+    public void somaDespesas(Double valorDespesas) {
+        BigDecimal valorConvertido = new BigDecimal(String.valueOf(valorDespesas));
+
+        this.valorDespesas = this.valorDespesas.add(valorConvertido).setScale(2, BigDecimal.ROUND_HALF_UP);
     }
 
     @Override
     public String toString() {
-        return String.format("%s, %s, %s, %s, %s", registroAns, cnpj, razaoSocial, modalidade, uf);
+        return String.format("%s, %s, %s, %s, %s, %s, %s", registroAns, cnpj, razaoSocial, modalidade, uf, valorDespesas, ano);
     }
 }
